98e2f45b6f5172865d50383e3a04d2d12c5016d7
Dutank <hyewon81543@gmail.com>
2019-06-17 23:27:26 +0200

    read xml file

--- parser/src/com/dhl/sds/XmlParser.java
+++ parser/src/com/dhl/sds/XmlParser.java
@@ -1,10 +1,14 @@
 package com.dhl.sds;
 
+import java.io.File;
+
 public class XmlParser {
 
 	public static void main(String[] args) {
 		
-
+		File xmlFolder = new File("/Users/dhl/sdsparser/parser/XMLData");
+		File[] xmlFiles = xmlFolder.listFiles();
+		File xmlFile = xmlFiles[0];
 	}
 
 }
